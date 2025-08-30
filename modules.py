b0d228c9798b9df439a585e18144a76f3f858b28
Vladyslav Senko <v.o.senko@gmail.com>
2025-08-30 16:57:15 +0300

    Use custom module

--- modules.py
+++ modules.py
@@ -16,3 +16,6 @@ num1 = 10
 num2 = 5
 
 print("Addition: ", mo.add(num1, num2))
+print("Subtraction: ", mo.subtr(num1, num2))
+print("Multiplication: ", mo.multiply(num1, num2))
+print("Division: ", mo.divide(num1, num2))
