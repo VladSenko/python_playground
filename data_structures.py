f53ee32a146cac6a67d9cf03e9002b6326b4cde5
Vladyslav Senko <v.o.senko@gmail.com>
2025-08-30 17:28:08 +0300

    tupples, dictionaries, sets

--- data_structures.py
+++ data_structures.py
@@ -27,3 +27,62 @@ print(fruits)
 
 fruits.pop()  # remove the last item from the list
 print(fruits)
+
+
+# Tupples
+colors = ("red", "green", "blue")  # can not be modified
+
+single_item = ("glass",)  # a single item Tuple should end with a comma
+
+print(colors[0])
+print(colors[-1])
+
+
+# Dictionaries
+
+student = {"name": "Alice", "age": 25, "smokes": True}
+print(student)
+print(student["name"])  # dictionary property access
+
+# modify and update dictionary
+student["subject"] = "Math"
+print(student)
+
+student["age"] = 30
+print(student)
+
+del student["smokes"]
+print(student)
+
+student.pop("subject")
+print(student)
+
+
+# Dictionary iteration
+for key, value in student.items():
+    print(key, value)
+
+
+# SETS - unordered collection of UNIQUE items (no duplicates)
+
+nums = {1, 2, 3, 4}
+
+empty_set = set()
+
+# adding and removing items to set
+nums.add(5)
+print(nums)
+
+nums.remove(2)
+print(nums)
+
+
+# Set operations: UNION, INTERSECTION, DIFFERENCE
+
+set_1 = {1, 2, 3}
+set_2 = {3, 4, 5}
+
+print(set_1 | set_2)  # union
+print(set_1 & set_2)  # intersection
+# difference - checks what does not exist in set_2 that exists in set_1
+print(set_1 - set_2)
