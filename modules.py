374aa6215926e55a56ecc62da9d86333bb9609c8
Vladyslav Senko <v.o.senko@gmail.com>
2025-08-30 16:56:26 +0300

    Use custom module

--- modules.py
+++ modules.py
@@ -1,11 +1,18 @@
-import math
+# import math
 
-from math import sqrt
+# from math import sqrt
 
-import math as m
+# import math as m
 
-print(math.sqrt(16))
+# print(math.sqrt(16))
 
-print(sqrt(25))
+# print(sqrt(25))
 
-print(m.sqrt(36))
+# print(m.sqrt(36))
+
+import math_operations as mo
+
+num1 = 10
+num2 = 5
+
+print("Addition: ", mo.add(num1, num2))
