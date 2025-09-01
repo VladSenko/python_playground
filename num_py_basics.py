e9bb15a072a20edb589af8eb70d32e3aae348385
vadyslav_senko <v.o.senko@gmail.com>
2025-09-01 16:01:09 +0300

    basic numpy operations

---
+++ num_py_basics.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr = np.array([1,2,3,4])
+print(arr)
+
+zeros = np.zeros((3,3))
+print(zeros)
+
+ones = np.ones((2,4))
+print(ones)
+
+range_array = np.arange(1, 20, 3)
+print(range_array)
+
+linspace_array = np.linspace(0, 10, 21)
+print(linspace_array)
